70fc0b2d4a2c41da40fd19ce17bf59541d69333a
BengIslam7 <devmlpentBMI66@gmail.com>
2024-07-19 15:16:37 +0100

    Updating launch file to run SLAM

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -4,11 +4,14 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
 
 import xacro
-
 
 def generate_launch_description():
 
@@ -17,9 +20,9 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
-    
+
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
@@ -29,13 +32,26 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    slam_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('slam_toolbox'),'launch','online_async_launch.py'])]),
+        launch_arguments={'slam_params_file':PathJoinSubstitution([FindPackageShare('my_bot'),'config','mapper_params_online_async.yaml']),'use_sim_time':'true'}.items()
+    )
+
+    gazebo_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('gazebo_ros'),'launch','gazebo.launch.py'])]),
+        launch_arguments={'world':PathJoinSubstitution([FindPackageShare('my_bot'),'worlds','my_world.world']),'use_sim_time':'true'}.items()
+    )
+    
+
+    
 
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
-        node_robot_state_publisher
+            description='Use sim time if true'
+        ),
+        node_robot_state_publisher,
+        slam_launch,
+        gazebo_launch
     ])
+
